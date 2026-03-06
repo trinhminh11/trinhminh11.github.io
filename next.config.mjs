ae2c8d529ca42d09de41d278d0f5c091a36e7bc3
Trịnh Thế Minh <72061760+trinhminh11@users.noreply.github.com>
2026-03-06 12:44:58 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  // Uncomment and update the line below if deploying to a subdirectory
-  // e.g., https://username.github.io/repo-name/
-  // basePath: '/repo-name',
-}
+  output: "export",
+  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH || "",
+};
 
-export default nextConfig
+export default nextConfig;
